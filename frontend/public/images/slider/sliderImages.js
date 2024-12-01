a085bec1d41adc7d24aa185c58497da8881583f7
Sonu Kumar <thesonukumar357@gmail.com>
2024-12-01 18:53:28 +0530

    second

--- frontend/public/images/slider/sliderImages.js
+++ frontend/public/images/slider/sliderImages.js
@@ -1,13 +1,11 @@
 import slide1 from './image1.png'
 import slider2 from './image2F.png'
 import slider3 from './image3F.png'
-import slider4 from './image22.jpeg'
-import slider5 from './image33.jpeg'
+import slider6 from './image44.webp'
 
-export const sliderImages= [
+export const sliderImages = [
      slide1,
-    slider2,
+     slider2,
      slider3,
-     slider4,
-     slider5
+     slider6
 ]
